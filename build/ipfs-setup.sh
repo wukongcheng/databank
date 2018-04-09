fcab70f70593c52d6b7c7a92c15cd0859849defd
chengsilei <chengsilei@qq.com>
2018-04-09 11:33:14 +0800

    ipfs : check whether the os is supported

--- build/ipfs-setup.sh
+++ build/ipfs-setup.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ x`uname`x = xDarwinx ]; then
+  echo 'Wanning: macOS is not supported.' >&2
+  exit 1
+fi
+
+if [ x`uname`x != xLinuxx ]; then
+  echo 'Wanning: only linux is supported.' >&2
+  exit 1
+fi
+
 if [ -x "$(command -v ipfs)" ]; then
   echo 'Wanning: ipfs is installed.' >&2
   exit 1
