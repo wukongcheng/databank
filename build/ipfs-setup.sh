572edb4ce7f38e0c4c572952ee897d6dbf312583
chengsilei <chengsilei@qq.com>
2018-04-09 16:31:42 +0800

    ipfs : private network

--- build/ipfs-setup.sh
+++ build/ipfs-setup.sh
@@ -12,12 +12,30 @@ fi
 
 if [ -x "$(command -v ipfs)" ]; then
   echo 'Wanning: ipfs is installed.' >&2
-  exit 1
+else
+  echo 'Installing ipfs ... ' >&1
+
+  tar -xvf go-ipfs_v0.4.14_linux-386.tar.gz
+  cd go-ipfs
+  sudo ./install.sh
+
+  cd ..
+  rm -rf go-ipfs
+
+  echo 'Done!' >&1
+
+  ipfs init
+  echo 'Init ipfs ... ' >&1
+
+  cp ./swarm.key ~/.ipfs
+  echo 'Set ipfs swarm.key ... ' >&1
+
+  ipfs bootstrap rm --all
+  ipfs bootstrap add /ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ
+  echo 'Reset ipfs bootstrap peers ... ' >&1
+
+  nohup ipfs daemon &
+  echo 'Start ipfs daemon ... ' >&1
+
 fi
 
-tar -xvf go-ipfs_v0.4.14_linux-386.tar.gz
-cd go-ipfs
-sudo ./install.sh
-
-cd ..
-rm -rf go-ipfs
